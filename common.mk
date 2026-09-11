94ffeb8b93998d3d8b880e03a49df043eedb2ce8
eightscrow <183341520+eightscrow@users.noreply.github.com>
2026-09-11 11:13:40 +0300

    sdm845-common: Disable overlays for the absent Lineage SDK

--- common.mk
+++ common.mk
@@ -28,9 +28,6 @@ PRODUCT_COPY_FILES += \
     $(LOCAL_PATH)/configs/public.libraries.txt:$(TARGET_COPY_OUT_VENDOR)/etc/public.libraries.txt
 
 # Overlays
-DEVICE_PACKAGE_OVERLAYS += \
-    $(LOCAL_PATH)/overlay-lineage
-
 PRODUCT_ENFORCE_RRO_TARGETS := *
 
 PRODUCT_PACKAGES += \
